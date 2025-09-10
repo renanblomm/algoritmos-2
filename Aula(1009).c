23e5fd3d076ff3efc93c949bdd0676d5a142924a
renanblomm <renan6990@gmail.com>
2025-09-10 08:00:35 -0300

    Update Aula(1009)

--- Aula(1009).c
+++ Aula(1009).c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#define LINHAS (4)
+#define COLUNAS (4)
 
+void populaMatriz(int linhas, int colunas, int m[linhas][colunas){
+  for (int i = 0, int < linhas, i++){
+    for (int j = 0, j < colunas, j++){
+    printf("M[%d][%d]: ", i, j);
+    scanf("%d", &m[i][j]);
+    }
+  }
+    }
